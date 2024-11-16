8f4d77732a9c0783761262dd81e134b1d767906e
Cass Unterholzner <cityboundforest@gmail.com>
2024-11-15 18:45:43 -0600

    Adding template repository

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![cfg_attr(test, feature(custom_test_frameworks))]
+#![cfg_attr(test, reexport_test_harness_main = "test_main")]
+#![cfg_attr(test, test_runner(agb::test_runner::test_runner))]
+
+#[agb::entry]
+fn main(mut gba: agb::Gba) -> ! {
+    agb::no_game(gba);
 }
