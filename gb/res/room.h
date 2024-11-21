c5684db3f7eec5e9a2f17a3b825649e499f9d948
Cass Unterholzner <cityboundforest@gmail.com>
2024-11-21 13:28:16 -0600

    Adding tilemap from TilemapStudio and its generated code

---
+++ gb/res/room.h
@@ -0,0 +1,10 @@
+#ifndef ROOM_H
+#define ROOM_H
+
+#define ROOM_TILE_WIDTH 20
+#define ROOM_TILE_HEIGHT 18
+
+extern const unsigned char room_tilemap[];
+extern const unsigned int room_len;
+
+#endif
