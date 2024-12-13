f46a5b2bc1bcd95fb1133882578d68addcfaa5f1
Cass Unterholzner <cityboundforest@gmail.com>
2024-12-13 14:33:52 -0600

    Working on door

--- gb/src/main/states/gameplay/gameplay-state.asm
+++ gb/src/main/states/gameplay/gameplay-state.asm
@@ -17,15 +17,76 @@ DrawRoom::
     ld hl, $9800
     ld bc, room_Tilemap_end - room_Tilemap
     call CopyDEIntoMemoryAtHL
+    call SetRoomDoors
     ret
-;    ld de, roomTileData
-;    ld hl, $9340
-;    ld bc, roomTileDataEnd - roomTileData
-;    call CopyDEIntoMemoryAtHL
-;    ld de, roomTileMap
-;    ld hl, $9800
-;    ld bc, roomTileMapEnd - roomTileMap
-;    jp CopyDEintoMemoryAtHL_With520Offset
+
+SetRoomDoors::
+    ld a, [wEntrance]
+    ld e, a
+    ld hl, wDungeonGrid
+    ld a, l
+    add e
+    ld l, a
+    ld a, [hl]
+    and $04
+    jp z, .FuncBody2
+    ; set north door
+    push hl
+    ld hl, $9800+$08
+    ld a, $37+$0f   ; Row 1
+    ld [hli], a
+    ld a, $38+$0f
+    ld [hli], a
+    ld a, $39+$0f
+    ld [hli], a
+    ld a, $32+$0f
+    ld [hli], a
+    ld de, $001c
+    add hl, de      ; Row 2
+    ld a, $43+$0f
+    ld [hli], a
+    ld a, $44+$0f
+    ld [hli], a
+    ld a, $45+$0f
+    ld [hli], a
+    ld a, $3e+$0f
+    ld [hli], a
+    add hl, de      ; Row 3
+    ld a, $4f+$0f
+    ld [hli], a
+    ld a, $50+$0f
+    ld [hli], a
+    ld a, $51+$0f
+    ld [hli], a
+    ld a, $4a+$0f
+    ld [hli], a
+    add hl, de      ; Row 4
+.FuncBody2
+    ld a, [hl]
+    and $08
+    jp z, .FuncBody3
+    ; set east door
+.FuncBody3
+    ld a, [hl]
+    and $10
+    jp z, .FuncBody4
+    ; set south door
+.FuncBody4
+    ld a, [hl]
+    and $20
+    jp z, .FuncBody5
+    ; set west door
+.FuncBody5
+    ld a, [hl]
+    and $40
+    jp z, .FuncBody6
+    ; set stair down
+.FuncBody6
+    ld a, [hl]
+    and $80
+    jp z, .FuncBody7
+.FuncBody7
+    ret
 
 UpdateGameplayState::
     call WaitForOneVBlank
